be8a7fd888755203c47400d31632aaf387873596
bolzhub <romain.chaussonnier@proton.me>
2025-11-01 08:51:02 +0100

    remove unecessary app/layout.jsx

--- app/[lang]/layout.jsx
+++ app/[lang]/layout.jsx
@@ -1,5 +1,24 @@
+import "../global.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+
+export const metadata = {
+  title: {
+    default: "Bolzano Crafts",
+    template: "%s | Bolzano Crafts",
+  },
+  description: "Crafts by Bolzano",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+    ],
+    shortcut: "/favicon/favicon.ico",
+    apple: "/favicon/apple-touch-icon.png",
+  },
+  manifest: "/favicon/site.webmanifest",
+  applicationName: "BCrafts",
+};
 
 export default async function LangLayout({ children, params }) {
   const { lang } = await params; // ⬅️ important !
