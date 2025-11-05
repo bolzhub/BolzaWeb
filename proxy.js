6bceb2eba92bf7871dd44718c7191357522baed3
bolzhub <romain.chaussonnier@proton.me>
2025-11-05 05:32:59 +0100

    Slight modification of proxy.js to make it less "middleware" but can't replace createMiddleware function

--- proxy.js
+++ proxy.js
@@ -1,42 +1,42 @@
 // proxy.js
-import { NextResponse } from 'next/server';
-import createMiddleware from 'next-intl/middleware';
+import { NextResponse } from 'next/server'
+import createMiddleware from 'next-intl/middleware'
 
-const locales = ['fr', 'en'];
-const defaultLocale = 'en';
+const locales = ['fr', 'en']
+const defaultLocale = 'en'
 
-const intlMiddleware = createMiddleware({
+const intlProxy = createMiddleware({
   locales,
   defaultLocale,
-});
+})
 
 export function proxy(request) {
-  const { nextUrl, headers } = request;
-  const pathname = nextUrl.pathname;
+  const { nextUrl, headers } = request
+  const pathname = nextUrl.pathname
 
   // Si l’URL commence déjà par une locale supportée, on laisse passer
-  const pathnameLocale = pathname.split('/')[1];
+  const pathnameLocale = pathname.split('/')[1]
   if (locales.includes(pathnameLocale)) {
-    return intlMiddleware(request);
+    return intlProxy(request)
   }
 
   // Sinon, détecter la langue du navigateur via header Accept-Language
-  const acceptLang = headers.get('accept-language');
-  let preferred = defaultLocale;
+  const acceptLang = headers.get('accept-language')
+  let preferred = defaultLocale
   if (acceptLang) {
     // prendre le premier segment avant la virgule
-    const firstLang = acceptLang.split(',')[0].split('-')[0];
+    const firstLang = acceptLang.split(',')[0].split('-')[0]
     if (locales.includes(firstLang)) {
-      preferred = firstLang;
+      preferred = firstLang
     }
   }
 
   // rediriger vers /{preferred}{pathname}
-  const url = new URL(request.url);
-  url.pathname = `/${preferred}${pathname}`;
-  return NextResponse.redirect(url);
+  const url = new URL(request.url)
+  url.pathname = `/${preferred}${pathname}`
+  return NextResponse.redirect(url)
 }
 
 export const config = {
   matcher: '/((?!api|_next|_static|favicon\\.ico|.*\\..*).*)',
-};
+}
