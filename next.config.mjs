ba4992c951a6598a60b74c55c6d83b9df95e607b
bolzhub <romain.chaussonnier@proton.me>
2025-11-05 04:30:55 +0100

    New config (next.config.mjs, package.json, i18n/request.js) to welcome i18n and intl

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
 const nextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: [
@@ -8,4 +11,4 @@ const nextConfig = {
   ],
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
