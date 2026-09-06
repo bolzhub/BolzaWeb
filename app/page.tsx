8e4031e1afc9318cc1c6ac97c0818b0d9aa2d638
Bolzano <romain.chaussonnier@proton.me>
2026-09-06 20:58:19 +0200

    background dark grey and title

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,50 @@
-import Image from "next/image";
-
 export default function Home() {
+  const images = [
+    "/data/Projets/ADR/Ane%C3%A9.png",
+    "/data/Projets/ADR/AutreImage.png",
+    "/data/Projets/ADR/EncoreUne.png",
+  ];
+
   return (
-    <main
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        width: "100vw",
-        margin: 0,
-      }}
-    >
-      <img
-        src="/data/Projets/ADR/Ane%C3%A9.png"
-        alt="Description : premier dessin"
+    <main style={{ margin: 0, backgroundColor: "#1a1a1a" }}>
+      <h1
         style={{
-          maxWidth: "100%",
-          maxHeight: "100%",
-          objectFit: "contain",
+          // position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          textAlign: "center",
+          color: "white",
+          margin: 0,
+          padding: "1rem 0",
+          // zIndex: 10,
         }}
-      />
+      >
+        Atelier Double Raisin
+      </h1>
+
+      {images.map((src, i) => (
+        <section
+          key={i}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            width: "100vw",
+          }}
+        >
+          <img
+            src={src}
+            alt={`Illustration ${i + 1}`}
+            style={{
+              maxWidth: "100%",
+              maxHeight: "100%",
+              objectFit: "contain",
+            }}
+          />
+        </section>
+      ))}
     </main>
   );
 }
